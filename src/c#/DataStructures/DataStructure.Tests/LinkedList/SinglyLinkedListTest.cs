d9c63ba2e8f3c3a95e4c8861854a6e72c4dcda98
Caio Colaiacovo Carneiro da Costa <caiohshot@gmail.com>
2020-09-30 23:59:02 -0400

    Refactoring tests and classes structures; Adding more functionalities to list

--- src/c#/DataStructures/DataStructure.Tests/LinkedList/SinglyLinkedListTest.cs
+++ src/c#/DataStructures/DataStructure.Tests/LinkedList/SinglyLinkedListTest.cs
@@ -1,10 +1,18 @@
-﻿using ExpectedObjects;
+﻿using DataStructures.LinkedList;
+using ExpectedObjects;
 using Xunit;
 
 namespace DataStructure.Tests.LinkedList
 {
     public class SinglyLinkedListTest
     {
+        private readonly SinglyLinkedList<string> _list;
+
+        public SinglyLinkedListTest()
+        {
+            _list = new SinglyLinkedList<string>();
+        }
+
         [Fact]
         public void Should_create_a_list_without_head()
         {
@@ -13,84 +21,114 @@ namespace DataStructure.Tests.LinkedList
             Assert.Null(list.Head);
         }
 
+        //TODO: Should not add invalid values
+
         [Fact]
-        public void Should_add_node_to_head_when_list_is_empty()
+        public void Should_add_to_head_when_list_is_empty()
         {
-            var list = new SinglyLinkedList<string>();
-            const string data = "Node data";
-            var node = new Node<string>
-            {
-                Data = data,
-                Next = null
-            };
-            var expectedHead = node;
+            const string value = "Node value";
+            var newHead = new Node<string>(value, null);
+            var expectedHead = newHead;
 
-            list.Add(data);
+            _list.Add(value);
 
-            var head = list.Head;
+            var head = _list.Head;
             expectedHead.ToExpectedObject().ShouldMatch(head);
         }
 
         [Fact]
-        public void Should_add_node_to_the_end_when_list_is_not_empty()
+        public void Should_add_to_heads_next_when_list_contains_only_head()
         {
-            var list = new SinglyLinkedList<string>();
-            const string headData = "Head data";
-            const string nodeData = "Node1 data";
-            const string newNodeData = "NewNode data";
-            list.Add(headData);
-            list.Add(nodeData);
-            var expectedNode = new Node<string>
-            {
-                Data = newNodeData,
-                Next = null
-            };
+            const string firstValue = "First value";
+            const string newValue = "New value";
+            _list.Add(firstValue);
+            var expectedNode = new Node<string>(newValue, null);
 
-            list.Add(newNodeData);
+            _list.Add(newValue);
 
-            var lastNode = list.Head.Next.Next;
+            var headsNextNode = _list.Head.Next;
+            expectedNode.ToExpectedObject().ShouldMatch(headsNextNode);
+        }
+
+        [Fact]
+        public void Should_add_to_the_end_when_list_is_not_empty()
+        {
+            const string firstValue = "First value";
+            const string secondValue = "Second value";
+            const string thirdValue = "Thirdvalue";
+            const string newValue = "New value";
+            _list.Add(firstValue);
+            _list.Add(secondValue);
+            _list.Add(thirdValue);
+            var expectedNode = new Node<string>(newValue, null);
+
+            _list.Add(newValue);
+
+            var lastNode = _list.Head.Next.Next.Next;
             expectedNode.ToExpectedObject().ShouldMatch(lastNode);
         }
 
         [Fact]
-        public void Should_replace_head_when_add_element_to_first_position()
+        public void Should_replace_head_when_add_value_to_first_position()
         {
+            const string firstValue = "First value";
+            const string secondValue = "Second value";
+            const string newFirstValue = "New first value";
+            _list.Add(firstValue);
+            _list.Add(secondValue);
+            var expectedHead = new Node<string>(newFirstValue, _list.Head);
 
+            _list.AddFirst(newFirstValue);
+
+            var newHead = _list.Head;
+            expectedHead.ToExpectedObject().ShouldMatch(newHead);
         }
 
-        private class SinglyLinkedList<T> where T : class
+        [Fact]
+        public void Should_add_first_to_head_when_list_is_empty()
         {
-            public Node<T> Head { get; private set; }
+            const string value = "First value";
+            var expectedHead = new Node<string>(value, null);
 
-            internal void Add(T data)
-            {
-                var newNode = new Node<T> { Data = data };
+            _list.AddFirst(value);
 
-                if (Head == null)
-                {
-                    Head = newNode;
-                    return;
-                }
-
-                AddNext(Head, newNode);
-            }
-
-            private void AddNext(Node<T> currentNode, Node<T> next)
-            {
-                if (currentNode.Next == null)
-                {
-                    currentNode.Next = next;
-                    return;
-                }
-
-                AddNext(currentNode.Next, next);
-            }
+            var head = _list.Head;
+            expectedHead.ToExpectedObject().ShouldMatch(head);
         }
 
-        private class Node<T> where T : class
+        [Fact]
+        public void Should_replace_head_when_add_before_first_value()
         {
-            public T Data { get; set; }
-            public Node<T> Next { get; set; }
+            const string firstValue = "First value";
+            const string newValue = "New value";
+            _list.Add(firstValue);
+            var expectedHead = new Node<string>(newValue, _list.Head);
+
+            _list.AddBefore(firstValue, newValue);
+
+            var head = _list.Head;
+            expectedHead.ToExpectedObject().ShouldMatch(head);
         }
+
+        [Fact]
+        public void Should_add_before_specific_value()
+        {
+            const string firstValue = "First value";
+            const string secondValue = "Second value";
+            const string thirdValue = "Third value";
+            const string newValue = "New value";
+            _list.Add(firstValue);
+            _list.Add(secondValue);
+            _list.Add(thirdValue);
+            var expectedNode = new Node<string>(newValue, _list.Head.Next.Next);
+
+            _list.AddBefore(thirdValue, newValue);
+
+            var node = _list.Head.Next.Next;
+            expectedNode.ToExpectedObject().ShouldMatch(node);
+        }
+
+        //TODO: Shuld not add before when list is empty
+        //TODO: Shoud not add before when value does not exist
     }
 }
---
+++ src/c#/DataStructures/DataStructures/LinkedList/SinglyLinkedList.cs
@@ -0,0 +1,63 @@
+﻿namespace DataStructures.LinkedList
+{
+    public class SinglyLinkedList<T> where T : class
+    {
+        public Node<T> Head { get; private set; }
+
+        public void Add(T data)
+        {
+            var newNode = new Node<T>(data, null);
+
+            if (Head == null)
+            {
+                Head = newNode;
+                return;
+            }
+
+            AddNext(Head, newNode);
+        }
+
+        public void AddFirst(T data)
+        {
+            var newHead = new Node<T>(data, Head);
+
+            Head = newHead;
+        }
+
+        public void AddBefore(T data, T newData)
+        {
+            var newNode = new Node<T>(newData, null);
+
+            if (Head.Value == data)
+            {
+                newNode.Next = Head;
+                Head = newNode;
+
+                return;
+            }
+
+            var nodeFound = FindNodeWithNextData(data, Head);
+            newNode.Next = nodeFound.Next;
+            nodeFound.Next = newNode;
+        }
+
+        private void AddNext(Node<T> currentNode, Node<T> next)
+        {
+            if (currentNode.Next == null)
+            {
+                currentNode.Next = next;
+                return;
+            }
+
+            AddNext(currentNode.Next, next);
+        }
+
+        private Node<T> FindNodeWithNextData(T data, Node<T> node)
+        {
+            if (node.Next.Value == data)
+                return node;
+
+            return FindNodeWithNextData(data, node.Next);
+        }
+    }
+}
